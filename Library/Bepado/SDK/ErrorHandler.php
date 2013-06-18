c169378358539539efe43c71aec0e0ba28bfe521
Heiner Lohaus <hl@shopware.de>
2013-06-18 16:23:53 +0200

    Update the sdk library

---
+++ Library/Bepado/SDK/ErrorHandler.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * This file is part of the Bepado SDK Component.
+ *
+ * @version $Revision$
+ */
+
+namespace Bepado\SDK;
+
+/**
+ * Base class for error handler implementations
+ *
+ * @version $Revision$
+ */
+abstract class ErrorHandler
+{
+    /**
+     * Handle error
+     *
+     * @param Struct\Error $error
+     * @return void
+     */
+    abstract public function handleError(Struct\Error $error);
+
+    /**
+     * Handle exception
+     *
+     * @param \Exception $exception
+     * @return void
+     */
+    abstract public function handleException(\Exception $exception);
+}

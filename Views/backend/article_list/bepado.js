01319f3535bc6e57ebd1a0a8e48b0e9342b8d7d9
Daniel Nögel <dn@shopware.de>
2013-10-14 10:37:42 +0200

    BEP-290 - Disable price editor in product listing for bepado products

--- Views/backend/article_list/bepado.js
+++ Views/backend/article_list/bepado.js
@@ -8,11 +8,62 @@
  */
 
 /**
- * Extend the article's base fieldSet and add out customizing field
+ * Extend the article list in order to show a bepado icon and disable the price field
  */
 //{block name="backend/article_list/view/main/grid" append}
 Ext.define('Shopware.apps.ArticleList.view.main.Grid-Customizing', {
     override: 'Shopware.apps.ArticleList.view.main.Grid',
+
+    /**
+     * @Override the init method in order to disable the editor for price field of bepado products
+     */
+    initComponent: function() {
+        var me = this;
+
+        me.callOverridden(arguments);
+
+        me.editor.on('beforeedit', function(editor, context, eOpts) {
+            var me = this;
+
+            if (!me.columns || !context.record) {
+                return;
+            }
+
+            if (!me.priceColumn) {
+                me.getPriceColumn();
+            }
+
+            // Disable the price column for bepado products
+            me.priceColumn.getEditor().setDisabled(context.record.get('bepado'));
+        }, me);
+    },
+
+    /**
+     * Helper to get the price column
+     */
+    getPriceColumn: function() {
+        var me = this,
+            i, currentColumn, columnsLength = me.columns.length;
+
+        // Save the default price editor and the price column
+        for (i = 0; i<columnsLength; i++) {
+            currentColumn =me.columns[i];
+
+            if (currentColumn.dataIndex == 'price') {
+                me.priceColumn = currentColumn;
+            }
+        }
+    },
+
+    /**
+     *
+     * @Override: Show a bepado icon for bepado products
+     *
+     * @param value
+     * @param metaData
+     * @param record
+     * @returns Object
+     */
     infoColumnRenderer: function(value, metaData, record) {
         var me = this,
             title,
@@ -27,5 +78,6 @@ Ext.define('Shopware.apps.ArticleList.view.main.Grid-Customizing', {
 
         return result;
     }
+
 });
 //{/block}

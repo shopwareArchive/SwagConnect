f83585d97dbe1b340dd6cea5c3f6b9d543a2945d
Daniel Nögel <dn@shopware.de>
2014-02-14 14:43:51 +0100

    NTR - Increase version to 1.3.4

--- Bootstrap.php
+++ Bootstrap.php
@@ -39,7 +39,7 @@ final class Shopware_Plugins_Backend_SwagBepado_Bootstrap extends Shopware_Compo
      */
     public function getVersion()
     {
-        return '1.3.3';
+        return '1.3.4';
     }
 
     /**

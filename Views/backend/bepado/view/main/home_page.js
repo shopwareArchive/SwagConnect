4015775ba968f843601bb9cd4cec17dcff43d62b
Daniel Nögel <dn@shopware.com>
2014-02-26 12:15:04 +0100

    BEP-621 - Move home page template to own file

--- Views/backend/bepado/view/main/home_page.js
+++ Views/backend/bepado/view/main/home_page.js
@@ -1,5 +1,7 @@
 //{namespace name=backend/bepado/view/main}
 
+// {include file="backend/bepado/_resources/html/home_page.tpl" assign="homePageContent"}
+
 //{block name='backend/bepado/view/main/home_page'}
 Ext.define('Shopware.apps.Bepado.view.main.HomePage', {
     extend: 'Ext.panel.Panel',
@@ -20,49 +22,7 @@ Ext.define('Shopware.apps.Bepado.view.main.HomePage', {
 
     getHTMLContent: function() {
         var me = this;
-        me.htmlTpl = [
-            '<div class="bepado-home-page">',
-                '<h3 class="headline">bepado – be part of it!</h3>',
-                '<div class="content">',
-                    '<p>',
-                    'bepado ist die erste soziale B2B-Handelsplattform der Welt. Ziel des Portals ist die Vernetzung von ',
-                    'Onlinehändlern untereinander, damit diese ihre Angebote durch die Sortimente anderer Teilnehmer ergänzen',
-                    'können. Das schafft, ganz im Sinne eines sozialen Netzwerks, eine größere Reichweite für den Einzelnen,',
-                    'sorgt für mehr Unabhängigkeit von großen Marktplätzen, erlaubt Dropshipping uvm. und bringt jedem ',
-                    'Teilnehmer unterm Strich mehr Umsatz ein. Kurzum: bepado ist ein Netzwerk, das den eCommerce nachhaltig',
-                    'stärken wird! Mit der Installation dieses Plugins sind Sie nur noch einen Schritt von bepado entfernt.<br/><br/>',
-                    'Nutzen Sie diese Erweiterung, um Ihre Artikel in das Netzwerk einzupflegen und Ihrerseits die für Sie ',
-                    'in Frage kommenden Artikel in Ihren Online-Shop zu importieren.<br/><br/>Im Detail unterstützt das Plugin folgende Funktionen:',
-                    '</p>',
-                    '<ul>',
-                        '<li>',
-                            '<strong>Produkt Import</strong>',
-                            '<p>Importieren Sie Ihre abonnierten Artikel in Ihren Shop, um Ihr Produktsortiment mit wenigen Handgriffen zu erweitern.</p>',
-                        '</li>',
-
-                        '<li>',
-                            '<strong>Produkt Export</strong>',
-                            '<p>Exportieren Sie beliebige Produkte nach bepado und stellen Sie diese anderen Shopbetreibern im Netzwerk zur Verfügung.</p>',
-                        '</li>',
-
-                        '<li>',
-                            '<strong>Kategoriezuweisung</strong>',
-                            '<p>Weisen Sie Ihren Kategorien eine von den vorgebenen Kategorien auf bepado zu.</p>',
-                        '</li>',
-
-                        '<li>',
-                            '<strong>Cloud Search</strong>',
-                            '<p>Durchsuchen Sie mit dieser ausgeklügelten Funktion das gesamte Netzwerk übergreifend nach für Sie in Frage kommenden Artikeln.</p>',
-                        '</li>',
-
-                        '<li>',
-                            '<strong>Versandarten ausschließen</strong>',
-                            '<p>Um bestimmte Versandarten nicht in Kombination mit bepado-Produkten zu erlauben, öffnen Sie einfach das Versandkosten-Modul und entfernen Sie unter »Erweiterte Einstellungen« den Haken bei »Für bepado-Produkte freigeben«</p>',
-                        '</li>',
-                    '</ul>',
-                '</div>',
-            '</div>'
-        ].join('');
+        me.htmlTpl = '{$homePageContent|replace:"\n":""|replace:"\r":""}';
 
         return me.htmlTpl;
     }

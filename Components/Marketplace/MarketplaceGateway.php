a1464794012c55709dcc92775d73ed63beccf02d
Stefan Bofirov <stefan.bofirov@dnhsoft.com>
2015-03-24 17:50:26 +0200

    [BEPNEU-2471] Fixed get local mapping for marketplace attribute

--- Components/Marketplace/MarketplaceGateway.php
+++ Components/Marketplace/MarketplaceGateway.php
@@ -138,7 +138,7 @@ class MarketplaceGateway
             return null;
         }
 
-        return $mappingModel->getMarketplaceAttribute();
+        return $mappingModel->getLocalAttribute();
     }
 
     /**

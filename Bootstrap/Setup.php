9169830ff8a7e0aa5bc9a0e7a3b11cc2b673c933
Martin Kafadarov <martin.kafadarov@dnhsoft.com>
2016-06-02 10:23:25 +0300

    CON-2871 Added names in s_core_states table for payment group

--- Bootstrap/Setup.php
+++ Bootstrap/Setup.php
@@ -684,15 +684,16 @@ class Setup
     public function populatePaymentStates()
     {
         $states = array(
-            'SC received',
-            'SC requested',
-            'SC instructed',
-            'SC aborted',
-            'SC timeout',
-            'SC pending',
-            'SC refunded',
-            'SC loss',
-            'SC error',
+            'sc_received' => ' SC received',
+            'sc_requested' => 'SC requested',
+            'sc_instructed' => 'SC instructed',
+            'sc_aborted' => 'SC aborted',
+            'sc_timeout' => 'SC timeout',
+            'sc_pending' => 'SC pending',
+            'sc_refunded' => 'SC refunded',
+            'sc_verify' => 'SC verify',
+            'sc_loss' => 'SC loss',
+            'sc_error' => 'SC error',
         );
 
         $query = Shopware()->Models()->getRepository('Shopware\Models\Order\Status')->createQueryBuilder('s');
@@ -709,10 +710,10 @@ class Setup
             }
         }
 
-        foreach ($states as $name) {
+        foreach ($states as $name => $description) {
             $isExists = Shopware()->Db()->query('
                 SELECT `id` FROM `s_core_states`
-                WHERE `description` = ?
+                WHERE `name` = ?
                 ', array($name)
             )->fetch();
 
@@ -723,9 +724,9 @@ class Setup
             $currentId++;
             Shopware()->Db()->query('
                 INSERT INTO `s_core_states`
-                (`id`, `description`, `position`, `group`, `mail`)
-                VALUES (?, ?, ?, ?, ?)
-                ', array($currentId, $name, $currentId, 'payment', 0)
+                (`id`, `name`, `description`, `position`, `group`, `mail`)
+                VALUES (?, ?, ?, ?, ?, ?)
+                ', array($currentId, $name, $description, $currentId, 'payment', 0)
             );
         }
     }

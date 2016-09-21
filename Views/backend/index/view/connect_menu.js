bb3b6d8be0ac213980a9bb3c91be16dbdbf71588
Martin Kafadarov <martin.kafadarov@dnhsoft.com>
2016-09-21 15:23:24 +0300

    CON-3851 Fixed plugin updater events initialization

--- Views/backend/index/view/connect_menu.js
+++ Views/backend/index/view/connect_menu.js
@@ -9,35 +9,34 @@ Ext.define('Shopware.apps.Index.view.ConnectMenu', {
     initComponent: function() {
         var me = this, result;
 
-        Shopware.app.Application.addSubApplication({
-                name: 'Shopware.apps.PluginManager',
-                params: {
-                    hidden: true
-                }
-            },
-            false,
-            function() {
-                var record = Ext.create('Shopware.apps.PluginManager.model.Plugin', {
-                    technicalName: 'SwagConnect'
-                });
-
-                var controller = Ext.create('Shopware.apps.PluginManager.controller.Plugin');
-
-                record.reload(function(swagConnect) {
-                    Shopware.Notification.createStickyGrowlMessage({
-                        title: '{s name=plugin/update/header}New Connect plugin update available.{/s}',
-                        text: '{s name=plugin/update/text}Please update your connect plugin.{/s}',
-                        width: 400,
-                        btnDetail: {
-                            text: '{s name=plugin/update/btn}Update{/s}',
-                            callback: function() {
-                                controller.updatePlugin(swagConnect[0]);
-                            }
-                        }
+            Shopware.app.Application.addSubApplication({
+                    name: 'Shopware.apps.PluginManager',
+                    params: {
+                        hidden: false
+                    }
+                },
+                false,
+                function() {
+                    var controller = Ext.create('Shopware.apps.PluginManager.controller.Plugin');
+                    var record = Ext.create('Shopware.apps.PluginManager.model.Plugin', {
+                        technicalName: 'SwagConnect'
                     });
-                });
-            }
-        );
+                    record.reload(function(swagConnect) {
+                        Shopware.Notification.createStickyGrowlMessage({
+                            title: '{s name=plugin/update/header}New Connect plugin update available.{/s}',
+                            text: '{s name=plugin/update/text}Please update your connect plugin.{/s}',
+                            width: 400,
+                            btnDetail: {
+                                text: '{s name=plugin/update/btn}Update{/s}',
+                                callback: function() {
+                                    controller.init();
+                                    Shopware.app.Application.fireEvent('update-plugin', swagConnect[0]);
+                                }
+                            }
+                        });
+                    });
+                }
+            );
 
         result = me.callParent(arguments);
 

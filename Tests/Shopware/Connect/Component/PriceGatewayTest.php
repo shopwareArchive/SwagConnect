9a8e5fbf0872e8f14937b52b90cceb3afa221a0d
Stefan Bofirov <stefan.bofirov@dnhsoft.com>
2016-10-25 12:10:37 +0300

    CON-3937 Fixed PriceGatewayTest

--- Tests/Shopware/Connect/Component/PriceGatewayTest.php
+++ Tests/Shopware/Connect/Component/PriceGatewayTest.php
@@ -27,6 +27,27 @@ class PriceGatewayTest extends \PHPUnit_Framework_TestCase
      */
     public function all_products_should_have_configured_price()
     {
+        // insert missing prices
+        $query = $query = Shopware()->Db()->query("
+                SELECT sad.articleID as articleId, sad.id as detailId
+                FROM s_articles_details sad
+                LEFT JOIN s_articles_prices sap ON sad.id = sap.articledetailsID AND sap.pricegroup = ?
+                LEFT JOIN s_plugin_connect_items spci ON sad.id = spci.article_detail_id
+                WHERE spci.shop_id IS NULL AND sap.price IS NULL OR sap.price = 0
+            ", ['EK']);
+
+        Shopware()->Db()->beginTransaction();
+
+        foreach ($query->fetchAll() as $record) {
+            Shopware()->Db()->executeQuery(
+                'INSERT INTO `s_articles_prices`(
+              `pricegroup`, `from`, `to`, `articleID`, `articledetailsID`, `price`
+              ) VALUES ("EK", 1, "beliebig", ?, ?, 50)', [$record['articleId'], $record['detailId']]
+            );
+        }
+
+        Shopware()->Db()->commit();
+
         $customerGroup = $this->customerGroupRepository->findOneBy(array('key' => 'EK'));
         $this->assertEquals(0, $this->priceGateway->countProductsWithoutConfiguredPrice($customerGroup, 'price'));
     }

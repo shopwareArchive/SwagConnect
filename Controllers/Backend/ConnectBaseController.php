169b11966668e6d462006b972e07c1bf87cb9070
Sven Finke <sven.finke@googlemail.com>
2016-10-25 15:00:33 +0200

    fix login issues

--- Controllers/Backend/ConnectBaseController.php
+++ Controllers/Backend/ConnectBaseController.php
@@ -616,7 +616,10 @@ class ConnectBaseController extends \Shopware_Controllers_Backend_ExtJs
             [
                 'content-type' => 'application/x-www-form-urlencoded'
             ],
-            'username=' . $shopwareId . '&password=' . $password
+            [
+                'username' => urlencode($shopwareId),
+                'password' => urlencode($password)
+            ]
         );
 
         $responseObject = json_decode($response->getBody());
@@ -679,9 +682,11 @@ class ConnectBaseController extends \Shopware_Controllers_Backend_ExtJs
             [
                 'content-type' => 'application/x-www-form-urlencoded'
             ],
-            'username=' . $shopwareId .
-            '&password=' . $password .
-            '&email=' . $email
+            [
+                'username'  => urlencode($shopwareId),
+                'password'  => urlencode($password),
+                'email'     => urlencode($email)
+            ]
         );
 
         $responseObject = json_decode($response->getBody());

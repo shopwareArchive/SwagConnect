e78c98611811a0687ae1c8cd02a8f866ca9a12c3
Stefan Stefanoff <stefan.stefanov@dnhsoft.com>
2016-12-22 11:35:03 +0200

    CON-4074 Removing stream connect status when stream is no longer exported to connect

--- Components/ProductFromShop.php
+++ Components/ProductFromShop.php
@@ -568,6 +568,7 @@ class ProductFromShop implements ProductFromShopBase
 
         try {
             $this->markStreamsAsSynced();
+            $this->markStreamsAsNotExported();
         } catch (\Exception $e) {
             $this->logger->write(
                 true,
@@ -576,6 +577,39 @@ class ProductFromShop implements ProductFromShopBase
             );
         }
 
+    }
+
+    private function markStreamsAsNotExported()
+    {
+        $streamIds = $this->manager->getConnection()->executeQuery(
+            "SELECT pcs.stream_id as streamId
+             FROM s_plugin_connect_streams as pcs
+             WHERE export_status = ?",
+            array(ProductStreamService::STATUS_DELETE)
+        )->fetchAll();
+
+        foreach ($streamIds as $stream) {
+            $streamId = $stream['streamId'];
+
+            $notDeleted = $this->manager->getConnection()->executeQuery(
+                "SELECT pss.id
+                 FROM s_product_streams_selection as pss
+                 JOIN s_plugin_connect_items as pci
+                 ON pss.article_id = pci.article_id
+                 WHERE pss.stream_id = ?
+                 AND pci.export_status != ?",
+                array($streamId, null)
+            )->fetchAll();
+
+            if (count($notDeleted) === 0) {
+                $this->manager->getConnection()->executeQuery(
+                    "UPDATE s_plugin_connect_streams
+                     SET export_status = ?
+                     WHERE stream_id = ?",
+                    array(null, $streamId)
+                );
+            }
+        }
     }
 
     private function markStreamsAsSynced()

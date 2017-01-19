93f7dabc2585e747b274938c8354926fc5dbbf86
Martin Kafadarov <martin.kafadarov@dnhsoft.com>
2017-01-19 17:25:14 +0200

    CON-4104 Updated SDK version

--- Library/Shopware/Connect/SDK.php
+++ Library/Shopware/Connect/SDK.php
@@ -60,7 +60,7 @@ final class SDK
     /**
      * Version constant
      */
-    const VERSION = '2.0.2';
+    const VERSION = '2.0.3';
 
     /**
      * @param string $apiKey API key assigned to you by Shopware Connect

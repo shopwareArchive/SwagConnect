f6dc454807c0817c25239b1c01acf7571eeb3052
Martin Kafadarov <martin.kafadarov@dnhsoft.com>
2017-01-30 17:26:45 +0200

    CON-4158 Added relation db table between dynamic streams and articles

--- Bootstrap/Setup.php
+++ Bootstrap/Setup.php
@@ -420,6 +420,13 @@ class Setup
               `export_status` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
               `export_message` text COLLATE utf8_unicode_ci DEFAULT NULL,
               PRIMARY KEY (`id`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;","
+            CREATE TABLE IF NOT EXISTS `s_plugin_connect_streams_relation` (
+                `stream_id` int(11) unsigned NOT NULL,
+                `article_id` int(11) unsigned NOT NULL,
+                UNIQUE KEY `stream_id` (`stream_id`,`article_id`),
+                CONSTRAINT s_plugin_connect_streams_selection_fk_stream_id FOREIGN KEY (stream_id) REFERENCES s_product_streams (id) ON DELETE CASCADE,
+                CONSTRAINT s_plugin_connect_streams_selection_fk_article_id FOREIGN KEY (article_id) REFERENCES s_articles (id) ON DELETE CASCADE
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
             ");
 

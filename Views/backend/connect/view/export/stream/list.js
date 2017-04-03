e76f1ca9a79decc1ee021d19e0da2ccc823ad891
Martin Kafadarov <martin.kafadarov@dnhsoft.com>
2017-04-03 19:19:01 +0300

    CON-4424 Added custom checkbox model

--- Views/backend/connect/view/export/stream/list.js
+++ Views/backend/connect/view/export/stream/list.js
@@ -9,10 +9,20 @@ Ext.define('Shopware.apps.Connect.view.export.stream.List', {
 
     store: 'export.StreamList',
 
-    selModel: {
-        selType: 'checkboxmodel',
-        mode: 'MULTI'
-    },
+    selModel: Ext.create('Shopware.apps.Connect.view.export.stream.CustomCheckboxModel', {
+        selectedAll: false, // you can add whatever normal configuration properties you want here
+        mode: 'MULTI',
+        listeners: {
+            selectall: function (scope) {
+                if (!scope.selectedAll) {
+                    scope.selectedAll = true;
+                } else {
+                    scope.deselectAll();
+                    scope.selectedAll = false;
+                }
+            }
+        }
+    }),
 
     initComponent: function() {
         var me = this;

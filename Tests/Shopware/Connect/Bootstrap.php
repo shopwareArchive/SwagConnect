f536f10509f08ce3fd6a9d81da5e4b53a528ab8d
Simon Bäumer <s.baeumer@shopware.com>
2017-05-29 09:32:56 +0200

    CON-4580 - Add testing environment

--- Tests/Shopware/Connect/Bootstrap.php
+++ Tests/Shopware/Connect/Bootstrap.php
@@ -2,11 +2,31 @@
 
 namespace Tests\ShopwarePlugins\Connect;
 
-if (file_exists('./../../../../../../tests/Shopware/TestHelper.php')) {
-    include('./../../../../../../tests/Shopware/TestHelper.php');
-} else {
-    include(__DIR__ . '/../../../../../../../../../tests/Functional/bootstrap.php');
-}
-Shopware()->Loader()->registerNamespace('Tests\ShopwarePlugins\Connect', __DIR__  . '/');
+require_once __DIR__ . '/../../../../../../../../../autoload.php';
 
-Shopware()->Container()->get('ConnectSDK');
+use Shopware\Kernel;
+
+class SwagConnectTestKernel
+{
+    public static function start()
+    {
+        $kernel = new Kernel(getenv('SHOPWARE_ENV') ?: 'testing', false);
+        $kernel->boot();
+
+        $container = $kernel->getContainer();
+        $container->get('plugins')->Core()->ErrorHandler()->registerErrorHandler(E_ALL | E_STRICT);
+
+        /** @var $repository \Shopware\Models\Shop\Repository */
+        $repository = $container->get('models')->getRepository('Shopware\Models\Shop\Shop');
+
+        $shop = $repository->getActiveDefault();
+        $shop->registerResources();
+
+        $_SERVER['HTTP_HOST'] = $shop->getHost();
+
+        Shopware()->Loader()->registerNamespace('Tests\ShopwarePlugins\Connect', __DIR__  . '/');
+        Shopware()->Container()->get('ConnectSDK');
+    }
+}
+
+SwagConnectTestKernel::start();

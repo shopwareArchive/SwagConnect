a02cf740ed7b7dfa021d3001eb9a05c6006bbcb8
Sven Finke <sven.finke@googlemail.com>
2017-08-30 07:56:06 +0200

    [CON-4828] fix test and add psh and php-stan

---
+++ DevOps/scripts/run-php-stan-dir.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+./phpstan analyse -c phpstan.neon -l 0 Bootstrap
+./phpstan analyse -c phpstan.neon -l 0 Bundle
+./phpstan analyse -c phpstan.neon -l 0 Commands
+./phpstan analyse -c phpstan.neon -l 0 Components
+./phpstan analyse -c phpstan.neon -l 0 Controllers
+./phpstan analyse -c phpstan.neon -l 0 Models
+./phpstan analyse -c phpstan.neon -l 0 Services
+./phpstan analyse -c phpstan.neon -l 0 Struct
+./phpstan analyse -c phpstan.neon -l 0 Subscribers
+./phpstan analyse -c phpstan.neon -l 0 Views

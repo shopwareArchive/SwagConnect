21ab2be01546751f722382f44cd5c29cf2656496
Daniel Beier <beier-daniel@gmx.net>
2017-09-05 14:31:07 +0200

    CON-4768 - added one more test

--- Tests/Unit/Components/CategoryResolver/DefaultCategoryResolverTest.php
+++ Tests/Unit/Components/CategoryResolver/DefaultCategoryResolverTest.php
@@ -8,6 +8,7 @@
 namespace ShopwarePlugins\Connect\Tests\Unit\Components\CategoryResolver;
 
 use Shopware\Components\Model\ModelManager;
+use Shopware\CustomModels\Connect\ProductToRemoteCategory;
 use Shopware\CustomModels\Connect\ProductToRemoteCategoryRepository;
 use Shopware\CustomModels\Connect\RemoteCategoryRepository;
 use ShopwarePlugins\Connect\Components\CategoryResolver\DefaultCategoryResolver;
@@ -31,29 +32,54 @@ class DefaultCategoryResolverTest extends AbstractConnectUnitTest
     private $manager;
 
     /**
+     * @var ProductToRemoteCategoryRepository
+     */
+    private $productToRemoteCategoryRepository;
+
+    /**
      * @before
      */
     public function prepareMocks()
     {
         $this->remoteCategoryRepository = $this->createMock(RemoteCategoryRepository::class);
         $this->manager = $this->createMock(ModelManager::class);
+        $this->productToRemoteCategoryRepository = $this->createMock(ProductToRemoteCategoryRepository::class);
 
         $this->defaultCategoryResolver = new DefaultCategoryResolver(
             $this->manager,
             $this->remoteCategoryRepository,
-            $this->createMock(ProductToRemoteCategoryRepository::class)
+            $this->productToRemoteCategoryRepository
         );
     }
 
     public function test_store_remote_categories_success()
     {
         $articleId = 4;
-        $categories = ['Category 1'];
+        $categories = ['Category 1' => 'Category 1'];
 
         $this->manager->expects($this->exactly(2))
             ->method('flush');
 
         $this->manager->expects($this->never())
+            ->method('remove');
+
+        $this->defaultCategoryResolver->storeRemoteCategories($categories, $articleId);
+    }
+
+    public function test_store_remote_categories_remove_category()
+    {
+        $articleId = 4;
+        $categories = ['Category 1' => 'Category 1'];
+
+        $productToRemoteCategory = new ProductToRemoteCategory();
+        $productToRemoteCategory->setArticleId($articleId);
+        $productToRemoteCategory->setConnectCategoryId('Category 2');
+
+        $this->productToRemoteCategoryRepository->method('getArticleRemoteCategories')
+            ->with($articleId)
+            ->willReturn([$productToRemoteCategory]);
+
+        $this->manager->expects($this->once())
             ->method('remove');
 
         $this->defaultCategoryResolver->storeRemoteCategories($categories, $articleId);

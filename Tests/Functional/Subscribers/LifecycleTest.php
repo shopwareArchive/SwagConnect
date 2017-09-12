c9487515f10c60046ab93b99b91c4b5664b9beb9
Stefan Bofirov <stefan.bofirov@dnhsoft.com>
2017-09-12 15:55:14 +0300

    CON-4792 Do not generate delete change for not exported product Test generation of delete changes. Must skip products which are not exported to Connect.

--- Tests/Functional/Subscribers/LifecycleTest.php
+++ Tests/Functional/Subscribers/LifecycleTest.php
@@ -8,7 +8,6 @@
 namespace ShopwarePlugins\Connect\Tests\Functional\Subscribers;
 
 use ShopwarePlugins\Connect\Tests\DatabaseTestCaseTrait;
-use Shopware\Models\Article\Detail;
 use Shopware\Components\Model\ModelManager;
 
 class LifecycleTest extends \Enlight_Components_Test_Plugin_TestCase
@@ -26,7 +25,6 @@ class LifecycleTest extends \Enlight_Components_Test_Plugin_TestCase
     public function prepare()
     {
         $this->manager = Shopware()->Models();
-
         // disable auth and acl
         Shopware()->Plugins()->Backend()->Auth()->setNoAuth();
         Shopware()->Plugins()->Backend()->Auth()->setNoAcl();
@@ -53,5 +51,33 @@ class LifecycleTest extends \Enlight_Components_Test_Plugin_TestCase
 
         $this->assertCount(1, $changes);
         $this->assertEquals('delete', $changes[0]['c_operation']);
+    }
+
+    public function test_generate_delete_change_only_for_exported()
+    {
+        $this->importFixtures(__DIR__ . '/_fixtures/simple_variants.sql');
+
+        $detailExist = $this->manager->getConnection()->fetchColumn(
+            'SELECT COUNT(id) FROM s_articles_details WHERE id = ? AND articleID = ?',
+            [2404544, 32871]
+        );
+        $this->assertEquals(1, $detailExist, 'Article detail does not exist!');
+
+        $this->Request()
+            ->setMethod('POST')
+            ->setPost('articleId', 32871)
+            ->setPost('id', 2404544);
+
+        $this->dispatch('backend/Article/deleteDetail');
+
+        $this->assertEquals(200, $this->Response()->getHttpResponseCode());
+        $this->assertTrue($this->View()->success);
+
+        $changes = $this->manager->getConnection()->fetchAll(
+            'SELECT c_entity_id, c_operation, c_revision, c_payload FROM sw_connect_change WHERE c_entity_id = ?',
+            ['32871-2404544']
+        );
+
+        $this->assertEmpty($changes);
     }
 }
--- Tests/Functional/Subscribers/_fixtures/simple_variants.sql
+++ Tests/Functional/Subscribers/_fixtures/simple_variants.sql
@@ -2,9 +2,24 @@ INSERT INTO s_articles (`id`, `name`, `main_detail_id`) VALUES (32870, 'All-New 
 INSERT INTO s_articles_details (id, `articleID`, `ordernumber`, `kind`) VALUES (2404535, 32870, 'sw32870', 1);
 INSERT INTO s_plugin_connect_items (article_id, article_detail_id, source_id, purchase_price_hash, offer_valid_until, stream, export_status)
 VALUES (32870, 2404535, 32870, "hash", 123, 1, 'synced');
+
 INSERT INTO s_articles_details (id, `articleID`, `ordernumber`, `kind`) VALUES (2404536, 32870, 'sw32870.2', 2);
 INSERT INTO s_plugin_connect_items (article_id, article_detail_id, source_id, purchase_price_hash, offer_valid_until, stream, export_status)
 VALUES (32870, 2404536, '32870-2404536', "hash", 123, 1, 'synced');
+
 INSERT INTO s_articles_details (id, `articleID`, `ordernumber`, `kind`) VALUES (2404537, 32870, 'sw32870.3', 2);
 INSERT INTO s_plugin_connect_items (article_id, article_detail_id, source_id, purchase_price_hash, offer_valid_until, stream, export_status)
 VALUES (32870, 2404537, '32870-2404537', "hash", 123, 1, 'synced');
+
+INSERT INTO s_articles (`id`, `name`, `main_detail_id`) VALUES (32871, 'E-book reader', 2404542);
+INSERT INTO s_articles_details (id, `articleID`, `ordernumber`, `kind`) VALUES (2404542, 32871, 'sw32871', 1);
+INSERT INTO s_plugin_connect_items (article_id, article_detail_id, source_id, purchase_price_hash, offer_valid_until, stream, export_status, exported)
+VALUES (32871, 2404542, '32871-2404542', "hash", 123, 1, '', 0);
+
+INSERT INTO s_articles_details (id, `articleID`, `ordernumber`, `kind`) VALUES (2404543, 32871, 'sw32871.2', 2);
+INSERT INTO s_plugin_connect_items (article_id, article_detail_id, source_id, purchase_price_hash, offer_valid_until, stream, export_status, exported)
+VALUES (32871, 2404543, '32871-2404543', "hash", 123, 1, '', 0);
+
+INSERT INTO s_articles_details (id, `articleID`, `ordernumber`, `kind`) VALUES (2404544, 32871, 'sw32871.3', 2);
+INSERT INTO s_plugin_connect_items (article_id, article_detail_id, source_id, purchase_price_hash, offer_valid_until, stream, export_status, exported)
+VALUES (32871, 2404544, '32871-2404544', "hash", 123, 1, '', 0);

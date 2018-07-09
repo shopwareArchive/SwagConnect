1ac0551b46c05052cd91178e96b5cc6a80bee756
Jonas Elfering <jonas.elfering@web.de>
2018-07-09 14:26:43 +0200

    fix image url

--- Tests/Legacy/Shopware/Connect/ConnectTestHelper.php
+++ Tests/Legacy/Shopware/Connect/ConnectTestHelper.php
@@ -40,7 +40,7 @@ class ConnectTestHelper extends \Enlight_Components_Test_Plugin_TestCase
      */
     private $connectFactory;
 
-    const IMAGE_PROVIDER_URL = 'http://www.shopware.de/ShopwareCommunityCenter/img/logo.png';
+    const IMAGE_PROVIDER_URL = 'https://de.shopware.com/press/company/Shopware_Jamaica.jpg';
 
     public function setUp()
     {
